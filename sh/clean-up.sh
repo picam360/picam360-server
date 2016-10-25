377429a6f359ffb9e185e152768a5288c6dcafa6
takumadx <hirayama.takuma@ohmydigifab.com>
2016-10-25 13:08:30 +0900

    clean

--- sh/clean-up.sh
+++ sh/clean-up.sh
@@ -1,5 +1,3 @@
-sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
-sudo rm /home/pi/.bash_history
 cd /home/pi/picam360/picam360-capture
 git remote set-url origin https://github.com/picam360/picam360-capture.git
 git reset --hard & git pull
@@ -7,3 +5,7 @@ make
 cd /home/pi/picam360/picam360-software
 git remote set-url origin https://github.com/picam360/picam360-software.git
 git reset --hard & git pull
+
+sudo rm /home/pi/.bash_history
+#need to be last because wifi connection will be disable
+sudo cp /etc/wpa_supplicant/wpa_supplicant.conf.init /etc/wpa_supplicant/wpa_supplicant.conf
