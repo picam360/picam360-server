1ac39e0ffb04de67d5ad6cb39f8b9bc62c82b566
takumadx <hirayama.takuma@ohmydigifab.com>
2016-10-25 12:43:28 +0900

    cleanup

---
+++ sh/clean-up.sh
@@ -0,0 +1,6 @@
+sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
+sudo rm /home/pi/.bash_history
+cd /home/pi/picam360/picam360-capture
+git remote set-url origin https://github.com/picam360/picam360-capture.git
+cd /home/pi/picam360/picam360-software
+git remote set-url origin https://github.com/picam360/picam360-software.git
