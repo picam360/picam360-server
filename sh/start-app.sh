bf0179a1f6d1cdadc827a6497042923a69519c23
takumadx <hirayama.takuma@ohmydigifab.com>
2017-01-04 18:46:24 +0900

    rename

--- sh/start-app.sh
+++ sh/start-app.sh
@@ -1,1 +1,1 @@
-/home/pi/.nodebrew/current/bin/node /home/pi/.nodebrew/current/bin/forever start -c '/home/pi/.nodebrew/current/bin/node --expose_gc' /home/pi/picam360/picam360-software/app.js > /dev/null
+/home/pi/.nodebrew/current/bin/node /home/pi/.nodebrew/current/bin/forever start -c '/home/pi/.nodebrew/current/bin/node --expose_gc' /home/pi/picam360/picam360-server/app.js 1> /tmp/app.log 2> /tmp/app.err
