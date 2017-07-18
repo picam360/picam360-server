e6820b77ba4556b26d42e23bdf187477eb06873d
takumadx <hirayama.takuma@ohmydigifab.com>
2017-07-18 14:29:53 +0900

    dev

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ process.chdir(__dirname);
 
 var os = require('os');
 var disk = require('diskusage');
-var OpenPilot = require('./openpilot.js');
 var child_process = require('child_process');
 var async = require('async');
 var fs = require("fs");
 var express = require('express');
-//var piblaster = require('pi-blaster.js');
 var moment = require("moment");
 var sprintf = require('sprintf-js').sprintf;
 
@@ -40,14 +38,8 @@ var GC_THRESH = 16*1024*1024;//16MB
 
 var capture_if;
 var capture_process;
-var op = new OpenPilot();
 async.waterfall([ function(callback) {// exit sequence
 	process.on('SIGINT', function() {
-		console.log("led shutting down");
-		//piblaster.setPwm(40, 0);
-		//piblaster.setPwm(41, 0);
-		// console.log("camera shutting down");
-		// cam1.stop();
 		console.log("exit process done");
 		process.exit();
 	});
@@ -64,32 +56,10 @@ async.waterfall([ function(callback) {// exit sequence
 		}
 	});
 	callback(null);
-}, function(callback) {// led startup
-	//console.log("led starting up");
-	//piblaster.setPwm(40, 0);
-	//piblaster.setPwm(41, 0);
+}, function(callback) {// connect to picam-capture
+	console.log("connect to picam-capture");
+	
 	callback(null);
-}, function(callback) {// capture startup
-	console.log("camera starting up");
-	child_process.exec('sudo killall raspivid', function() {
-		child_process.exec('sudo killall picam360-capture.bin', function() {
-			//var cmd = 'bash ../picam360-capture/lunch.sh -w 2048 -h 2048 -f 5 -W 480 -H 600 -r -n 2 -c MJPEG -S';
-			var cmd = 'bash ../picam360-capture/lunch.sh -w 2048 -h 2048 -f 5 -W 480 -H 600 -c MJPEG -S';
-			capture_process = child_process.exec(cmd, function (error, stdout, stderr) {
-			    //if(stdout){
-			    //    console.log('stdout: ' + stdout);
-			    //}
-			    //if(stderr){
-			    //    console.log('stderr: ' + stderr);
-			    //}
-			    //if (error !== null) {
-			    //	console.log('Exec error: ' + error);
-			    //}
-			});
-			capture_if = capture_process.stdin;
-			callback(null);
-		});
-	});
 }, function(callback) {//cam
 	console.log("camera instance");
 	
@@ -107,27 +77,9 @@ async.waterfall([ function(callback) {// exit sequence
 		}
 	}, 100);
 	callback(null);
-}, function(callback) {// connect to openpilot
-	//op.init(function() {
-	//	callback(null);
-	//});
-	callback(null);
-}, function(callback) {// connect to openpilot
-	//op.connect(function() {
-	//	callback(null);
-	//});
-	callback(null);
 }, function(callback) {// start up websocket server
 	console.log("websocket server starting up");
 
-	var veicle_attitude = {
-		// -180 - 180
-		Pitch : 0,
-		// -180 - 180
-		Yaw : 0,
-		// -180 - 180
-		Roll : 0,
-	};
 	var app = require('express')();
 	var http = require('http').Server(app);
 	var io = require("socket.io").listen(http);
@@ -199,29 +151,6 @@ async.waterfall([ function(callback) {// exit sequence
 	
 	app.use(express.static('www'));//this need be set after all dynamic files
 
-	var yaw_offset = 0;
-	var controlValue = {
-		// 0% - 100%
-		Throttle : 0,
-		// -180 - 180
-		Pitch : 0,
-		// -180 - 180
-		Yaw : 0,
-		// -180 - 180
-		Roll : 0,
-	};
-	var accel_factor = 0.02;
-
-	function degToOne(value) {
-		value = value % 180;// -180 <-> +180
-		return value / 180;// -1 <-> +1
-	}
-	function degToRad(value) {
-		return Math.PI * value / 180;
-	}
-	function radToDeg(value) {
-		return 180 * value / Math.PI;
-	}
 	function clone(src) {
 		var dst = {}
 		for ( var k in src) {
@@ -233,97 +162,6 @@ async.waterfall([ function(callback) {// exit sequence
 	io.sockets.on("connection", function(socket) {
 
 		socket.on("connected", function() {
-		});
-
-		socket.on("set_view_orientation", function(orientation) {
-			var id = 0;
-			var cmd = sprintf('set_view_orientation %d=%f,%f,%f\n', id, orientation.Pitch, orientation.Yaw, orientation.Roll);
-			//console.log(cmd);
-			capture_if.write(cmd);
-		});
-
-		socket.on("setFov", function(fov) {
-			var id = 0;
-			var cmd = sprintf('set_fov %d=%f\n', id, fov);
-			//console.log(cmd);
-			capture_if.write(cmd);
-		});
-
-		socket.on("accelerate_throttle", function(value, callback) {
-			controlValue.Throttle += accel_factor * value;
-			if (controlValue.Throttle < 0.0) {
-				controlValue.Throttle = 0.0;
-			} else if (controlValue.Throttle > 1.0) {
-				controlValue.Throttle = 1.0;
-			}
-			op.setThrust(controlValue.Throttle, function() {
-				callback();
-			});
-		});
-
-		socket.on("connectFcm", function(callback) {
-			op.connect(function() {
-				callback();
-			});
-		});
-
-		socket.on("setArm", function(bln, callback) {
-			controlValue.Throttle = 0.0;
-			op.setArm(bln, function(res) {
-				callback(res);
-			});
-		});
-
-		socket.on("calibrateLevel", function(callback) {
-			var LEVEL_SAMPLES = 100;
-			op.calibrateLevel(LEVEL_SAMPLES, function(res) {
-				op.getObject("AttitudeState", function(attitude) {
-					yaw_offset = attitude.Yaw;
-					console.log("yaw_offset : " + yaw_offset);
-					callback(res);
-				});
-			});
-		});
-
-		socket.on("setControlValue", function(value, callback) {
-			controlValue = value;
-			callback();
-		});
-
-		socket.on("getControlValue", function(callback) {
-			callback(controlValue);
-		});
-
-		socket.on("setActuatorValue", function(value, callback) {
-			op.setActuator(value, function(res) {
-				callback(res);
-			});
-		});
-
-		socket.on("getActuatorValue", function(callback) {
-			op.getObject("ActuatorCommand", function(obj) {
-				callback(obj);
-			}, true);
-		});
-
-		socket.on("getAttitude", function(callback) {
-			op.getObject("AttitudeState", function(obj) {
-				var dst = clone(obj);
-				dst.Yaw -= yaw_offset;
-				callback(dst);
-			}, true);
-		});
-
-		socket.on("setUdpProxyEnabled", function(value) {
-			op.setUdpProxyEnabled(value);
-		});
-
-		socket.on("setUpperLedValue", function(value) {
-			//piblaster.setPwm(40, value / 100.0);
-		});
-
-		socket.on("setBottomLedValue", function(value) {
-			//piblaster.setPwm(41, value / 100.0);
 		});
 
 		socket.on("snap", function(callback) {
@@ -372,31 +210,6 @@ async.waterfall([ function(callback) {// exit sequence
 				});
 		});
 
-		socket.on("startAC", function() {
-			var cmd = 'start_ac\n';
-			capture_if.write(cmd);
-			console.log(cmd);
-		});
-
-		socket.on("stopAC", function(callback) {
-			var cmd;
-			cmd = 'save\n';
-			capture_if.write(cmd);
-			cmd = 'stop_ac\n';
-			capture_if.write(cmd);
-			console.log(cmd);
-		});
-
-		socket.on("isRecording", function(callback) {
-			if(recording) {
-				callback(true);
-			} else {
-				fs.exists('/tmp/movie.h264', function(exists) { 
-					callback(exists);
-				}); 
-			}			
-		});
-
 		socket.on("disconnect", function() {
 		});
 
@@ -406,25 +219,6 @@ async.waterfall([ function(callback) {// exit sequence
 	  console.log('listening on *:9001');
 	});
 	
-	callback(null);
-}, function(callback) {// start up websocket server
-	console.log("poling start!");
-	var step = 0;
-	setInterval(function() {
-		step++;
-		switch (step % 2) {
-		case 0:
-			op.getObject("FlightStatus", function(res) {
-				// console.log(res);
-			}, true);
-			break;
-		case 1:
-			op.getObject("FlightTelemetryStats", function(res) {
-				// console.log(res);
-			}, true);
-			break;
-		}
-	}, 1000);
 	callback(null);
 } ], function(err, result) {
 });
