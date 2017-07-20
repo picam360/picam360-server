626ba056514fc7b54951b7fea131063b85d0d925
takumadx <hirayama.takuma@ohmydigifab.com>
2017-07-20 13:52:57 +0900

    rtp

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var fs = require("fs");
 var express = require('express');
 var moment = require("moment");
 var sprintf = require('sprintf-js').sprintf;
-var dgram = require("dgram");
+var rtp = require("rtp.js");
 
 function watchFile(filepath, oncreate, ondelete) {
 	var fs = require('fs'), path = require('path'), filedir = path
@@ -61,120 +61,9 @@ async
 						function(callback) {// connect to picam-capture
 							console.log("connect to picam-capture");
 
-							var rtp_rx = dgram.createSocket("udp4");
+							rtp.rtp_client(9004, function(pack) {
 
-							rtp_rx.on("error", function(err) {
-								console.log("server error:\n" + err.stack);
-								server.close();
 							});
-
-							var marker = 0;
-							var packet = null;
-							var xmp = false;
-							var xmp_len = 0;
-							var xmp_pos = 0;
-							rtp_rx.on("message", function(buff, rinfo) {
-								var data_len = buff.length;
-								for (var i = 0; i < data_len; i++) {
-									if (xmp) {
-										if (xmp_pos == 2) {
-											xmp_len = buff[i];
-											xmp_pos++;
-										} else if (xmp_pos == 3) {
-											xmp_len += buff[i] << 8;
-											xmp_pos++;
-										} else if (xmp_pos == 4) {
-											if (buff[i] == 0x72 ) {// r
-												xmp_pos++;
-											} else {
-												xmp = false;
-											}
-										} else if (xmp_pos == 5) {
-											if (buff[i] == 0x74) { // t
-												xmp_pos++;
-											} else {
-												xmp = false;
-											}
-										} else if (xmp_pos == 6) {
-											if (buff[i] == 0x70 ) {// p
-												xmp_pos++;
-											} else {
-												xmp = false;
-											}
-										} else if (xmp_pos == 7) { // rtp_header
-											if (buff[i] == 0x00) { // \0
-												xmp_pos++;
-											} else {
-												xmp = false;
-											}
-										} else {
-											if (xmp_pos == 8) {
-												pack = new Buffer(xmp_len - 8);
-											}
-											if (i + (xmp_len - xmp_pos) <= data_len) {
-												buff.copy(pack, xmp_pos - 8, i,
-														i + (xmp_len - xmp_pos));
-												i += xmp_len - xmp_pos - 1;
-												xmp_pos = xmp_len;
-												console.log("packet : " + xmp_len);
-// pack->LoadHeader();
-// if (lg_callback && lg_load_fd < 0) {
-// lg_callback(pack->GetPayloadData(),
-// pack->GetPayloadLength(),
-// pack->GetPayloadType(),
-// pack->GetSequenceNumber());
-// }
-// if (lg_record_fd > 0) {
-// pthread_mutex_lock(&lg_record_packet_queue_mlock);
-// lg_record_packet_queue.push_back(pack);
-// mrevent_trigger(&lg_record_packet_ready);
-// pthread_mutex_unlock(&lg_record_packet_queue_mlock);
-// } else {
-// delete pack;
-// }
-												pack = null;
-												xmp = false;
-											} else {
-												var rest_in_buff = data_len - i;
-												buff.copy(pack, xmp_pos - 8, i,
-														i + rest_in_buff);
-												i = data_len - 1;
-												xmp_pos += rest_in_buff;
-											}
-										}
-									} else {
-										if (marker) {
-											marker = 0;
-											if (buff[i] == 0xE1) { // xmp
-												xmp = true;
-												xmp_pos = 2;
-												xmp_len = 0;
-											}
-										} else if (buff[i] == 0xFF) {
-											marker = 1;
-										}
-									}
-								}
-							});
-
-							rtp_rx.on("listening",
-									function() {
-										var address = rtp_rx.address();
-										console.log("server listening "
-												+ address.address + ":"
-												+ address.port);
-									});
-
-							rtp_rx.bind(9004);
-
-							var rtcp_tx = dgram.createSocket("udp4");
-
-							rtcp_tx.on("error", function(err) {
-								console.log("server error:\n" + err.stack);
-								rtcp_tx.close();
-							});
-
-							// rtcp_tx.bind(9005);
 
 							callback(null);
 						},
---
+++ rtp.js
@@ -0,0 +1,128 @@
+var dgram = require("dgram");
+
+function PacketHeader(pack) {
+
+	var packetlength = pack.length;
+	var payloadtype = pack.readUInt8(1) & 0x7F;
+	var sequencenumber = pack.readUInt16BE(2);
+	var timestamp = pack.readUInt16BE(4);
+	var self = {
+		GetSequenceNumber : function() {
+			return sequencenumber;
+		},
+		GetTimestamp : function() {
+			return timestamp;
+		},
+		GetPacketData : function() {
+			return pack;
+		},
+		GetHeaderLength : function() {
+			return 12;
+		},
+		GetPayloadLength : function() {
+			return pack;
+		},
+		GetPayloadType : function() {
+			return payloadtype;
+		}
+	};
+	return self;
+}
+
+function rtp_client(port, callback) {
+
+	var rtp_rx = dgram.createSocket("udp4");
+
+	rtp_rx.on("error", function(err) {
+		console.log("server error:\n" + err.stack);
+		server.close();
+	});
+
+	var marker = 0;
+	var packet = null;
+	var xmp = false;
+	var xmp_len = 0;
+	var xmp_pos = 0;
+	rtp_rx.on("message", function(buff, rinfo) {
+		var data_len = buff.length;
+		for (var i = 0; i < data_len; i++) {
+			if (xmp) {
+				if (xmp_pos == 2) {
+					xmp_len = buff[i];
+					xmp_pos++;
+				} else if (xmp_pos == 3) {
+					xmp_len += buff[i] << 8;
+					xmp_pos++;
+				} else if (xmp_pos == 4) {
+					if (buff[i] == 0x72) {// r
+						xmp_pos++;
+					} else {
+						xmp = false;
+					}
+				} else if (xmp_pos == 5) {
+					if (buff[i] == 0x74) { // t
+						xmp_pos++;
+					} else {
+						xmp = false;
+					}
+				} else if (xmp_pos == 6) {
+					if (buff[i] == 0x70) {// p
+						xmp_pos++;
+					} else {
+						xmp = false;
+					}
+				} else if (xmp_pos == 7) { // rtp_header
+					if (buff[i] == 0x00) { // \0
+						xmp_pos++;
+					} else {
+						xmp = false;
+					}
+				} else {
+					if (xmp_pos == 8) {
+						pack = new Buffer(xmp_len - 8);
+					}
+					if (i + (xmp_len - xmp_pos) <= data_len) {
+						buff
+								.copy(pack, xmp_pos - 8, i, i
+										+ (xmp_len - xmp_pos));
+						i += xmp_len - xmp_pos - 1;
+						xmp_pos = xmp_len;
+
+						console.log("packet : " + xmp_len);
+						callback(PacketHeader(pack));
+
+						pack = null;
+						xmp = false;
+					} else {
+						var rest_in_buff = data_len - i;
+						buff.copy(pack, xmp_pos - 8, i, i + rest_in_buff);
+						i = data_len - 1;
+						xmp_pos += rest_in_buff;
+					}
+				}
+			} else {
+				if (marker) {
+					marker = 0;
+					if (buff[i] == 0xE1) { // xmp
+						xmp = true;
+						xmp_pos = 2;
+						xmp_len = 0;
+					}
+				} else if (buff[i] == 0xFF) {
+					marker = 1;
+				}
+			}
+		}
+	});
+
+	rtp_rx.on("listening",
+			function() {
+				var address = rtp_rx.address();
+				console.log("server listening " + address.address + ":"
+						+ address.port);
+			});
+
+	rtp_rx.bind(port);
+}
+
+exports.rtp_client = rtp_client;
