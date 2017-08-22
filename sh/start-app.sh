adb22890d3c710e7981337a9dfe25520fff869d0
takumadx <hirayama.takuma@ohmydigifab.com>
2017-08-22 12:21:43 +0900

    --minUptime 10000

--- sh/start-app.sh
+++ sh/start-app.sh
@@ -1,1 +1,1 @@
-/home/pi/.nodebrew/current/bin/node /home/pi/.nodebrew/current/bin/forever start -c '/home/pi/.nodebrew/current/bin/node --expose_gc' /home/pi/picam360/picam360-server/app.js 1> /tmp/app.log 2> /tmp/app.err
+/home/pi/.nodebrew/current/bin/node /home/pi/.nodebrew/current/bin/forever start --minUptime 10000 -c '/home/pi/.nodebrew/current/bin/node --expose_gc' /home/pi/picam360/picam360-server/app.js 1> /tmp/app.log 2> /tmp/app.err
