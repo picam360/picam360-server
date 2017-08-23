9bc545acde321bf2ea0efa46ca300091d4cee3f6
takumadx <hirayama.takuma@ohmydigifab.com>
2017-08-23 11:56:32 +0900

    rm some more files

--- sh/clean-up.sh
+++ sh/clean-up.sh
@@ -2,17 +2,26 @@ cd /home/pi/picam360/picam360-capture
 git remote set-url origin https://github.com/picam360/picam360-capture.git
 git reset --hard & git pull
 make
+cd /home/pi/picam360/picam360-driver
+git remote set-url origin https://github.com/picam360/picam360-driver.git
+git reset --hard & git pull
+make
 cd /home/pi/picam360/picam360-server
 sudo rm -r userdata
 mkdir userdata
 git remote set-url origin https://github.com/picam360/picam360-server.git
 git reset --hard & git pull
 
-sudo rm /home/pi/.bash_history
-sudo rm /home/pi/.nano_history
-sudo rm /home/pi/.gdb_history
+echo "rm history files"
+sudo rm /home/pi/.*_history
 sudo rm /home/pi/.gitconfig
+sudo rm /home/pi/.xsession-errors*
+sudo rm /home/pi/picam360-capture/.picam360_history
+sudo rm /home/pi/picam360-driver/.picam360_history
+
+echo "reset rc.local"
 sudo cp /etc/rc.local.server /etc/rc.local
+
 #need to be last because wifi connection will be disable
 echo "disable wpa? [y/N]"
 read WPA
