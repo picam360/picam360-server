8e89fb2da6bc1fd21cea1c3c0a9f3a7b9b06fa0a
takumadx <hirayama.takuma@ohmydigifab.com>
2017-08-23 11:40:55 +0900

    rm some more files

--- sh/clean-up.sh
+++ sh/clean-up.sh
@@ -9,6 +9,9 @@ git remote set-url origin https://github.com/picam360/picam360-server.git
 git reset --hard & git pull
 
 sudo rm /home/pi/.bash_history
+sudo rm /home/pi/.nano_history
+sudo rm /home/pi/.gdb_history
+sudo rm /home/pi/.gitconfig
 sudo cp /etc/rc.local.server /etc/rc.local
 #need to be last because wifi connection will be disable
 echo "disable wpa? [y/N]"
