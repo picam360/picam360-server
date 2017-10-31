cffad553c96d5a8ae9e933a54e21ae416aae342f
takumadx <hirayama.takuma@ohmydigifab.com>
2017-10-31 12:11:25 +0900

    tune forever param

--- sh/start-app.sh
+++ sh/start-app.sh
@@ -1,1 +1,2 @@
-/home/pi/.nodebrew/current/bin/node /home/pi/.nodebrew/current/bin/forever start --minUptime 10000 -c '/home/pi/.nodebrew/current/bin/node --expose_gc' /home/pi/picam360/picam360-server/app.js 1> /tmp/app.log 2> /tmp/app.err
+sudo killall node
+/home/pi/.nodebrew/current/bin/node /home/pi/.nodebrew/current/bin/forever start --minUptime 10000 --spinSleepTime 10000 -c '/home/pi/.nodebrew/current/bin/node --expose_gc' /home/pi/picam360/picam360-server/app.js 1> /tmp/app.log 2> /tmp/app.err
