567bcb447dc7685f35a69662edc2217277ed4579
takumadx <hirayama.takuma@ohmydigifab.com>
2018-06-12 12:04:47 +0900

    usvc:get_way_point

--- plugins/usvc.js
+++ plugins/usvc.js
@@ -11,42 +11,55 @@ module.exports = {
 		var DELIMITER = '\r\n';
 
 		var sp = null;
+		var max_way_point_num = 0;
 
-		async.waterfall([function(callback) {
-			var sp_callback = null;
-			sp = new SerialPort(COM_PORT, {
-				baudRate : 9600
-			});
-			var parser = sp.pipe(new SerialPort.parsers.Readline({
-				delimiter : DELIMITER
-			}));
-			function sp_send(str, _callback) {
-				sp.write(new Buffer(str + DELIMITER), function() {
-					sp_callback = _callback;
-					sp.drain();
+		async.waterfall([
+			function(callback) {
+				var sp_callback = null;
+				sp = new SerialPort(COM_PORT, {
+					baudRate : 9600
 				});
-			}
-			sp.on("open", function() {
-				setInterval(function() {
-					sp_send("get_max_way_point_num", function(ret) {
-						console.log("get_max_way_point_num:" + ret);
+				var parser = sp.pipe(new SerialPort.parsers.Readline({
+					delimiter : DELIMITER
+				}));
+				function sp_send(str, _callback) {
+					sp.write(new Buffer(str + DELIMITER), function() {
+						sp_callback = _callback;
+						sp.drain();
 					});
-				}, 1000);
-			});
-			parser.on("data", function(data) {
-				var ret = data.toString('utf-8', 0, data.length);
-				if (ret.startsWith("INFO:")) {
-					console.log(ret);
-				} else if (ret.startsWith("ERROR:")) {
-					console.log(ret);
-				} else if (sp_callback != null) {
-					sp_callback(ret);
 				}
-			});
-			callback(null);
-		}, function(callback) {
-			callback(null);
-		}], function(err, result) {
+				sp.on("open", function() {
+					setInterval(function() {
+						sp_send("get_max_way_point_num", function(ret) {
+							console.log("get_max_way_point_num:" + ret);
+							max_way_point_num = ret;
+							function get_way_point_walkthrough(idx) {
+								sp_send("get_way_point " + idx, function(ret) {
+									console.log("get_way_point " + idx + ":"
+										+ ret);
+									if (idx + 1 < max_way_point_num) {
+										get_way_point_walkthrough(idx + 1);
+									}
+								});
+							};
+							get_way_point_walkthrough(0);
+						});
+					}, 1000);
+				});
+				parser.on("data", function(data) {
+					var ret = data.toString('utf-8', 0, data.length);
+					if (ret.startsWith("INFO:")) {
+						console.log(ret);
+					} else if (ret.startsWith("ERROR:")) {
+						console.log(ret);
+					} else if (sp_callback != null) {
+						sp_callback(ret);
+					}
+				});
+				callback(null);
+			}, function(callback) {
+				callback(null);
+			}], function(err, result) {
 		});
 
 		var plugin = {
