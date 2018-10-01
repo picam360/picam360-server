1f47121181947b925eb1f490cb3cf3681d500a8b
takumadx <hirayama.takuma@ohmydigifab.com>
2018-10-01 09:55:22 +0900

    start-app.sh

--- sh/start-app.sh
+++ sh/start-app.sh
@@ -1,2 +1,2 @@
 sudo killall node
-/home/pi/.nodebrew/current/bin/node /home/pi/.nodebrew/current/bin/forever start --minUptime 10000 --spinSleepTime 10000 -c '/home/pi/.nodebrew/current/bin/node --expose_gc' /home/pi/picam360/picam360-server/app.js 1> /tmp/app.log 2> /tmp/app.err
+/home/$USER/.nodebrew/current/bin/node /home/$USER/.nodebrew/current/bin/forever start --minUptime 10000 --spinSleepTime 10000 -c "/home/$USER/.nodebrew/current/bin/node --expose_gc" /home/$USER/picam360/picam360-server/app.js 1> /tmp/app.log 2> /tmp/app.err
